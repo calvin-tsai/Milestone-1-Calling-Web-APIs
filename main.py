2033498b0243d64820bee38011a70b0b4cb3c470
Toprakim <66851631+Toprakim@users.noreply.github.com>
2023-06-08 05:46:29 +0000

    Adding conditions and random option for pokemon

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ print("Welcome to the Pokemon API!\n")
 name = ""
 while(name != "q"):
   name = input("Type a pokemon name or type r for a random pokemon to learn about (q = quit): ")
+  if(name == "r"):
+    name = random.choice(pokemonlist)
+    print(f"Your pokemon is {name}.")
